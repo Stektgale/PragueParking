e1b6f64a2338ad0315349167b15ef7bea772eaf5
Stektgale <stektgale.music@gmail.com>
2021-09-29 15:38:14 +0200

    Add project files.

---
+++ PragueParking Ver1.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PragueParking_Ver1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] parkingGarage = new string[100];
+        }
+
+        void ProcessInput()
+        {
+
+        }
+
+        void AddVehicle()
+        {
+
+        }
+
+        void RemoveVehicle()
+        {
+
+        }
+        void MoveVehicle()
+        {
+
+        }
+
+        void SearchForVehicle()
+        {
+
+        }
+
+
+    }
+}
