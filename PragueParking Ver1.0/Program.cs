35d729d219a453f8507c3bbbfab996fd56adf606
Stektgale <stektgale.music@gmail.com>
2021-09-29 16:38:48 +0200

    yeah

--- PragueParking Ver1.0/Program.cs
+++ PragueParking Ver1.0/Program.cs
@@ -4,35 +4,51 @@ namespace PragueParking_Ver1._0
 {
     class Program
     {
+        static string[] parkingGarage = new string[100];
+
         static void Main(string[] args)
         {
-            string[] parkingGarage = new string[100];
+            Console.WriteLine("Skriv pwease :3");
+            ProcessInput();
         }
 
-        void ProcessInput()
+        static void ProcessInput()
+        {
+            Console.Write("Input a command: ");
+            string input = Console.ReadLine();
+            switch (input.ToUpper())
+            {
+                case "ADD":
+                    AddVehicle();
+                    break;
+
+                default:
+                    Console.WriteLine("The input Was not a valid command. Input the help command for help list.");
+                    ProcessInput();
+                    break;
+            }
+        }
+
+        static void AddVehicle()
+        {
+            Console.Write("Select Parking Spot From 1-100: ");
+            int i = int.Parse(Console.ReadLine());
+
+            parkingGarage[i] = Console.ReadLine();
+        }
+
+        static void RemoveVehicle()
+        {
+
+        }
+        static void MoveVehicle()
         {
 
         }
 
-        void AddVehicle(int i)
-        {
-            
-        }
-
-        void RemoveVehicle()
+        static void SearchForVehicle()
         {
 
         }
-        void MoveVehicle()
-        {
-
-        }
-
-        void SearchForVehicle()
-        {
-
-        }
-
-
     }
 }
